9c57abb3a69999105b94bebbd6d9445b1f6aa21f
who-biz <37732338+who-biz@users.noreply.github.com>
2022-10-17 20:08:19 +0000

    Set NODE_NSPV service flag when '-nspv_msg' startup flag set

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -87,7 +87,7 @@ CAddress::CAddress(CService ipIn, uint64_t nServicesIn) : CService(ipIn)
 
 void CAddress::Init()
 {
-    nServices = NODE_NETWORK;
+    nServices = GetBoolArg("-nspv_msg", DEFAULT_NSPV_PROCESSING) ? NODE_NETWORK | NODE_NSPV : NODE_NETWORK;
     nTime = 100000000;
 }
 
