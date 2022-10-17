ecc9905d3326eab820fb47c4da30232660230106
who-biz <37732338+who-biz@users.noreply.github.com>
2022-10-17 19:39:34 +0000

    Add logging to 'nSPV_req' func to mask operation clarity

--- src/komodo_nSPV_superlite.h
+++ src/komodo_nSPV_superlite.h
@@ -231,6 +231,7 @@ CNode *NSPV_req(CNode *pnode,uint8_t *msg,int32_t len,uint64_t mask,int32_t ind)
         n = 0;
         BOOST_FOREACH(CNode *ptr,vNodes)
         {
+            LogPrintf(">>> (%s) ptr->nServices(%d), nServices & mask(%d), mask(%d), ptr->addr(%s)\n", __func__, ptr->nServices, (ptr->nServices & mask), mask, ptr->addr.ToString());
             if ( ptr->prevtimes[ind] > timestamp )
                 ptr->prevtimes[ind] = 0;
             if ( ptr->hSocket == INVALID_SOCKET )
