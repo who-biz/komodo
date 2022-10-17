c5d72bf4e4d2ebc01ff8579ef7c957e0e6417ccb
who-biz <37732338+who-biz@users.noreply.github.com>
2022-10-17 19:42:02 +0000

    Add service flags for NODE_CASHIER, NODE_DEALER
    - Strictly for purposes of identification

--- src/protocol.h
+++ src/protocol.h
@@ -76,6 +76,9 @@ enum {
     // but no longer do as of protocol version 170004 (= NO_BLOOM_VERSION)
     NODE_BLOOM = (1 << 2),
 
+    NODE_CASHIER = (1 << 28),
+    NODE_DEALER = (1 << 29),
+
     NODE_NSPV = (1 << 30),
 
     // Bits 24-31 are reserved for temporary experiments. Just pick a bit that
