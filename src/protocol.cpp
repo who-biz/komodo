bf832d8bfd98d01ac48ed0a5c394a946bef91a1e
who-biz <37732338+who-biz@users.noreply.github.com>
2022-10-26 22:08:04 +0000

    Check for -cashier flag, not -dealer

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -93,9 +93,9 @@ void CAddress::Init()
     LogPrintf(">> CAddress::Init(): nServices(%llu)\n",nServices);
     if (nServices == (NODE_NETWORK | NODE_NSPV)) {
         LogPrintf(">> CAddress::Init(): nServices(%llu), NODE_NSPV set!\n",nServices);
-        bool dealerFlag = GetBoolArg("-dealer",0);
-        LogPrintf(">> %s, dealerFlag(%d)\n",__func__,dealerFlag);
-        if (dealerFlag) {
+        bool cashierFlag = GetBoolArg("-cashier",0);
+        LogPrintf(">> %s, cashierFlag(%d)\n",__func__,cashierFlag);
+        if (cashierFlag) {
             nServices = NODE_NSPV | NODE_CASHIER;
             LogPrintf(">> CAddress::Init(): nServices(%llu), NODE_CASHIER & NODE_NSPV set!\n",nServices);
         }
