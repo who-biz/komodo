c7f9d82d919db06f855ce8e37f40350d8451cdd7
who-biz <37732338+who-biz@users.noreply.github.com>
2022-11-10 20:32:39 +0000

    Add logging to 'chipsgameresp' function to figure out crash loca

--- src/komodo_nSPV_superlite.h
+++ src/komodo_nSPV_superlite.h
@@ -224,7 +224,9 @@ void chips_gameresp(CNode *pfrom,std::vector<uint8_t> response) // received a re
     int32_t len; uint32_t timestamp = (uint32_t)time(NULL);
     if ( (len= response.size()) > 0 ) {
         if ( response[0] == CHIPS_GAMEDATARESP) {
+            LogPrintf(">>> (%s): prior to gamedata_purge...\n",__func__);
             CHIPS_gamedata_purge(&CHIPS_gamedataresult);
+            LogPrintf(">>> (%s): prior to rwgamedata...\n",__func__);
             CHIPS_rwgamedataresp(0,&response[1],&CHIPS_gamedataresult,len);
             LogPrintf(">>> %s: got CHIPS gamedata response %u size.%d retcode.%d\n",__func__,timestamp,(int32_t)response.size(),CHIPS_gamedataresult.retcode);
         } else {
