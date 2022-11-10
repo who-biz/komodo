1ca8d34cd2dae2fff06662f43a45be7f8acd3eb8
who-biz <37732338+who-biz@users.noreply.github.com>
2022-11-10 17:35:04 +0000

    Comment out spammy logging in CAddress::Init()

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -88,24 +88,13 @@ CAddress::CAddress(CService ipIn, uint64_t nServicesIn) : CService(ipIn)
 
 void CAddress::Init()
 {
-    LogPrintf(">> CAddressInit(): nServices(%llu) ... NODE_NETWORK example\n",NODE_NETWORK);
+//    LogPrintf(">> CAddressInit(): nServices(%llu) ... NODE_NETWORK example\n",NODE_NETWORK);
     nServices = GetBoolArg("-nspv_msg", DEFAULT_NSPV_PROCESSING) ? NODE_NETWORK | NODE_NSPV : NODE_NETWORK;
-    LogPrintf(">> CAddress::Init(): nServices(%016x), after nSPV flag check\n",nServices);
+//    LogPrintf(">> CAddress::Init(): nServices(%016x), after nSPV flag check\n",nServices);
     nServices = GetBoolArg("-cashier", DEFAULT_CASHIER_FLAG) ? nServices | NODE_CASHIER : nServices;
-    LogPrintf(">> CAddress::Init(): nServices(%016x), after cashier flag check\n",nServices);
+//    LogPrintf(">> CAddress::Init(): nServices(%016x), after cashier flag check\n",nServices);
     nServices = GetBoolArg("-dealer", DEFAULT_DEALER_FLAG) ? nServices | NODE_DEALER : nServices;
-    LogPrintf(">> CAddress::Init(): nServices(%016x), after dealer flag check\n",nServices);
-/*
-    if (nServices == (NODE_NETWORK | NODE_NSPV)) {
-        LogPrintf(">> CAddress::Init(): nServices(%016x), NODE_NSPV set!\n",nServices);
-        bool cashierFlag = GetBoolArg("-cashier",0);
-        LogPrintf(">> %s, cashierFlag(%d)\n",__func__,cashierFlag);
-        if (cashierFlag) {
-            nServices =| NODE_CASHIER;
-            LogPrintf(">> CAddress::Init(): nServices(%016x), NODE_CASHIER & NODE_NSPV set!\n",nServices);
-        }
-    }
-*/
+//    LogPrintf(">> CAddress::Init(): nServices(%016x), after dealer flag check\n",nServices);
     nTime = 100000000;
 }
 
