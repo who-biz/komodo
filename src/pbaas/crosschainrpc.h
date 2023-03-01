0e89cc5993293266bc3cda0035b2c5e5afcfc2c8
who-biz <37732338+who-biz@users.noreply.github.com>
2023-03-01 19:04:40 +0000

    Move PBAAS_TESTFORK_TIME to 1 July 2023

--- src/pbaas/crosschainrpc.h
+++ src/pbaas/crosschainrpc.h
@@ -28,7 +28,7 @@ static const int DEFAULT_RPC_TIMEOUT=900;
 static const uint32_t PBAAS_VERSION = 1;
 static const uint32_t PBAAS_VERSION_INVALID = 0;
 
-static const uint32_t PBAAS_TESTFORK_TIME = 1604216000;
+static const uint32_t PBAAS_TESTFORK_TIME = 1688216000;
 //static const uint32_t PBAAS_TESTFORK_TIME = 1677608188;
 
 class CTransaction;
